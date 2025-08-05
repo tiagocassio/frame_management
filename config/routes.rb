7d66475b3aa0dba2ac4600607dbc124da4d65ecd
Tiago Cássio <tiagocassio@outlook.com>
2025-08-05 12:31:55 -0300

    Setup default rubocop configuration.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  mount Rswag::Ui::Engine => "/api-docs"
-  mount Rswag::Api::Engine => "/api-docs"
-  get "up" => "rails/health#show", as: :rails_health_check
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
+  get 'up' => 'rails/health#show', as: :rails_health_check
 end
